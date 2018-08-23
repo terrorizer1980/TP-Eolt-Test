e705e664cf32280dff84d0e773edf914004a7802
LookForwardPersistence <fqhua_5@sina.cn>
2018-08-23 21:58:13 +0800

    test

--- js/main.js
+++ js/main.js
@@ -342,10 +342,10 @@ app = new Vue({
         },
         init_tokenpocket:function () {
             if(this.tpConnected){
-                /*tp.getWalletList("eos").then(function (data) {
+                tp.getWalletList("eos").then(function (data) {
                     this.tpAccount = data.wallets.eos[0]
-                });*/
-                tp.getCurrentWallet().then(function (data) {
+                });
+               /* tp.getCurrentWallet().then(function (data) {
                     this.notification('succeeded', JSON.parse(data));
                     if(data.result){
                         this.tpAccount = data.data;
@@ -353,7 +353,7 @@ app = new Vue({
                         this.notification("error",data.msg);
                     }
 
-                });
+                });*/
             }else {
                 this.notification('succeeded', '请下载TP,并登陆');
             }
@@ -536,7 +536,7 @@ async function requestId() {
        //移动端
        app.tpConnected=tp.isConnected();
           if(app.tpConnected){
-              await tp.getCurrentWallet().then(function (data) {
+            /*  await tp.getCurrentWallet().then(function (data) {
                   app.notification('succeeded', JSON.parse(data));
                   if(data.result){
                       app.tpAccount = data.data;
@@ -545,7 +545,11 @@ async function requestId() {
                       app.notification("error",data.msg);
                   }
 
-           });
+           });*/
+              await tp.getWalletList("eos").then(function (data) {
+                  app.tpAccount = data.wallets.eos[0]
+                  app.tpBalance();
+              });
           }else{
               app.notification('succeeded', '请下载TokenPocket或打开');
           }
