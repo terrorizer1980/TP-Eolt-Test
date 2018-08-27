1daf70302a632c9e8c8b42bc93706b1fec22b487
lychees <lychees67@gmail.com>
2018-08-28 04:42:39 +0800

    update test net.

--- js/config.js
+++ js/config.js
@@ -28,15 +28,15 @@ if(ENV === 'dev'){
     network = {
         blockchain: 'eos',
         host: 'api-kylin.eosasia.one',
-        port: 80,
+        port: 443,
         chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
-        protocol: "http"
+        protocol: "https"
     };
     options = {
         broadcast: true,
         sign: true,
         chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
-        httpEndpoint: "http://api-kylin.eosasia.one:80"
+        httpEndpoint: "https://api-kylin.eosasia.one:443"
     };
  } else if( ENV === 'mainnet'){
     // mainnet
