2ef7c0866cfa189c2fbfd177a945e67c9b02968e
LookForwardPersistence <fqhua_5@sina.cn>
2018-09-01 06:37:05 +0800

    price

--- js/main.js
+++ js/main.js
@@ -29,7 +29,8 @@ app = new Vue({
         tpConnected: false,
         eop: 1.000, // 经营状况系数
         user_hpy_balance:0,
-        tpAccount: null
+        tpAccount: null,
+        current_price:0
     },
     created: function () {},
     watch: {},
@@ -72,6 +73,7 @@ app = new Vue({
                     limit: 10,
                     table: 'market'
                 });
+
             happyeosslot_balance = happyeosslot_balance[0].split(' ', 1)[0];
             //this.eop = happyeosslot_true_balance;
             happyeosslot_true_balance = happyeosslot_true_balance.rows[0].deposit.balance.split(' ', 1)[0];
@@ -98,8 +100,12 @@ app = new Vue({
             });
             // happyeosslot_balance = happyeosslot_balance[0].split(' ', 1)[0];
             //this.eop = happyeosslot_true_balance;
+
             happyeosslot_true_balance = happyeosslot_true_balance.data.rows[0].deposit.balance.split(' ', 1)[0];
             this.eop = happyeosslot_balance / (happyeosslot_true_balance - 1250);
+            var nums= happyeosslot_true_balance.data.rows[0].supply.split(' ')
+            var num = nums[0];//happyeosslot_true_balance.data.rows[0].supply.split(' ', 1)[0];
+            this.current_price = (num/25000)*0.1*this.eop;
             //this.eop = new Number(this.eop).toFixed(4);
             return this.eop;
         },
